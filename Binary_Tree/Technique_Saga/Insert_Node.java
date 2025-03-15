f1e03e45ecbd7008bb85c2766dc739bf4baea53f
Shivam Bhargava <38013354+07shivam@users.noreply.github.com>
2025-03-16 04:07:47 +0530

    insertion in bt

--- Binary_Tree/Technique_Saga/Insert_Node.java
+++ Binary_Tree/Technique_Saga/Insert_Node.java
@@ -1,5 +1,72 @@
 package Binary_Tree.Technique_Saga;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import Core.TreeNode;
+
 public class Insert_Node {
-    
+
+    public TreeNode insertIntoBinaryTree(TreeNode root, int val) {
+        if(root == null)
+        {
+            return root;
+        }
+        TreeNode head = root;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                System.out.print(node.val + " ");
+                if(node.left == null)
+                {
+                    node.left = new TreeNode(val);
+                    return head;
+                }
+
+                if(node.right == null)
+                {
+                    node.right = new TreeNode(val);
+                    return head;
+                }
+
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+        }
+
+        return root;
+    }
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        
+        TreeNode current = root;
+        
+        while (true) {
+            if (val < current.val) {
+                if (current.left == null) {
+                    current.left = new TreeNode(val);
+                    break;
+                } else {
+                    current = current.left;
+                }
+            } else {
+                if (current.right == null) {
+                    current.right = new TreeNode(val);
+                    break;
+                } else {
+                    current = current.right;
+                }
+            }
+        }
+        
+        return root;
+    }
 }
